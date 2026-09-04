4b9dea6370f16ad1b361e853517f7cfad0c8a56d
yini-19 <yiyakazahnic@gmail.com>
2026-09-04 18:30:22 +0100

    connect python program with database

--- test_connection.py
+++ test_connection.py
@@ -5,14 +5,12 @@ import psycopg2
 load_dotenv()
 #api_key = os.getenv("OPENAI_API_KEY")
 database_url = os.getenv("DATABASE_URL")
-print("DATABASE_URL is:", repr(database_url))
-
 
 if not database_url:
     raise ValueError("DATABASE_URL not found - check your .env file")
 
 try:
-    connection=psycopg2.connect()
+    connection=psycopg2.connect(database_url)
     cursor=connection.cursor()
     cursor.execute("SELECT version()")
     result=cursor.fetchone()
